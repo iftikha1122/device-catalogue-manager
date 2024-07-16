25b33de29ac5b1fda755324e6747c91bbb864775
admin-cognovacrafts <iftikhar.hussain@vivy.com>
2024-07-17 00:04:08 +0200

    project structure and init h2db

---
+++ src/main/java/com/devices/catalogue/manager/DevicesCatalogueManagerApplication.java
@@ -0,0 +1,13 @@
+package com.devices.catalogue.manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevicesCatalogueManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevicesCatalogueManagerApplication.class, args);
+    }
+
+}
